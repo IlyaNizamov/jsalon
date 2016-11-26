426926af149ebaede515563fff3e4946683bf83e
Ilya Nizamov <info@niko1c.ru>
2016-11-26 15:41:56 +0500

    1

--- connect1c.js
+++ connect1c.js
@@ -1,12 +1,18 @@
-function resp1c() {
+function setupFrom1C() {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://localhost/salonserv/hs/eh/data", true);
+    xhr.open("GET", "http://localhost/salonserv/hs/eh/city", true);
     xhr.withCredentials = true;
     xhr.setRequestHeader("Authorization", 'Basic ' + btoa('admin:1'));
     xhr.onload = function () {
         if (xhr.status = 200) {
-            document.getElementById("response1C").innerHTML = xhr.responseText;
+            document.getElementById("SelectTown").innerHTML = xhr.responseText;
         }
     };
     xhr.send();
+}
+
+function reloadYaMap() {
+    let sel = document.getElementById("SelectTown");
+    let val = sel.options[sel.selectedIndex].value;
+    alert(val);
 }
