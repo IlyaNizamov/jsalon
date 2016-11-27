bb4460246e232197eabbe3d276ed4fad9eae2271
Admin <pr.nizamov@mail.ru>
2016-11-27 20:49:14 +0500

    Получаем данные городов с 1С

--- connect1c.js
+++ connect1c.js
@@ -12,7 +12,19 @@ function setupFrom1C() {
 }
 
 function reloadYaMap() {
+
     let sel = document.getElementById("SelectTown");
     let val = sel.options[sel.selectedIndex].value;
-    alert(val);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost/salonserv/hs/eh/city?city=" + val, true);
+    xhr.withCredentials = true;
+    xhr.setRequestHeader("Authorization", 'Basic ' + btoa('admin:1'));
+    xhr.onload = function () {
+        if (xhr.status = 200) {
+            cityData = JSON.parse(xhr.responseText);
+            myMap.setCenter([cityData.N, cityData.E],cityData.M);
+        }
+    };
+    xhr.send();
 }
